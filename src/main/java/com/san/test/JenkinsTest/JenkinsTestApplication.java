3aac986e00c3349481b40858e0a46bc1eba5343d
Sangram Konde <sangramkonde@gmail.com>
2018-09-26 05:09:32 -0500

    Added logger

--- src/main/java/com/san/test/JenkinsTest/JenkinsTestApplication.java
+++ src/main/java/com/san/test/JenkinsTest/JenkinsTestApplication.java
@@ -1,13 +1,18 @@
 package com.san.test.JenkinsTest;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsTestApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(JenkinsTestApplication.class);
+
 	public static void main(String[] args) {
-	    System.out.println("Starting Spring Boot application...");
+		LOGGER.info("Starting Spring Boot application...");
 		SpringApplication.run(JenkinsTestApplication.class, args);
 	}
 }
